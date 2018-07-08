a4bfc32be1c24684fa1d1e0745d343d63378fa32
sebastianroche <sebastian@softdev.team>
2018-07-08 16:03:31 -0500

    Change message

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello world!');
+    response.send('Hello world Jenkins!');
 });
 
 app.get('/demo-service', (request, response) => {
